665a854f12c0f57e46c4ef58fcf3db8b4500e711
klupzor <klupzor@gmail.com>
2020-08-12 04:15:19 +0000

    task 0

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,23 @@
+interface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string
+}
+
+const st1: Student = {
+    firstName: 'Pedro',
+    lastName: 'Calderon',
+    age: 50,
+    location: 'Bga',
+  }
+  
+  const st2: Student = {
+    firstName: 'Camilo',
+    lastName: 'Perez',
+    age: 50,
+    location: 'Bog',
+  }
+
+  const studentsList = [st1, st2]
+  
