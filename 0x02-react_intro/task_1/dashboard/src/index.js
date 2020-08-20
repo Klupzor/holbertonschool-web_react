d3a31a06f3f3874b63631b0485cd0d01079f8b2a
klupzor <klupzor@gmail.com>
2020-08-20 04:36:22 +0000

    task 2

--- 0x02-react_intro/task_1/dashboard/src/index.js
+++ 0x02-react_intro/task_1/dashboard/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Notifications from './Notifications';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Notifications />
   </React.StrictMode>,
   document.getElementById('root')
 );
