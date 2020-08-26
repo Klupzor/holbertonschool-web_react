608a968e14cc304ed5828107685ca08f7b2f95b0
klupzor <klupzor@gmail.com>
2020-08-26 03:25:34 +0000

    task 3

---
+++ 0x02-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label>Email: <input type="text" name="email"></input></label>
+        <label>Password: <input type="password" name="password"></input></label>
+        <button>OK</button>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
