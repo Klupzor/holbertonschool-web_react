76ae20c9221450916e5a7173006336a585c4d7f4
klupzor <klupzor@gmail.com>
2020-09-01 03:00:51 +0000

    adding tests

--- 0x0a_react_props/task_1/dashboard/src/Login/Login.test.js
+++ 0x0a_react_props/task_1/dashboard/src/Login/Login.test.js
@@ -3,6 +3,17 @@ import { shallow } from 'enzyme';
 import Login from './Login';
 
 test('renders a div with the class App-body', () => {
-    const wrapper = shallow(<Login />);
-    expect(wrapper.find('#app_body')).toHaveClassName('App-body');
-  });
+  const wrapper = shallow(<Login />);
+  expect(wrapper.find('#app_body')).toHaveClassName('App-body');
+});
+
+
+test('renders two input tags', () => {
+  const wrapper = shallow(<Login />);
+
+  const inputs = wrapper.find('input');
+  const labels = wrapper.find('label');
+
+  expect(inputs.length).toBe(2);
+  expect(labels.length).toBe(2);
+});
